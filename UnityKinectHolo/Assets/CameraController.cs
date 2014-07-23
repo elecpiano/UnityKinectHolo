6b4f3a248bd3aa9ec02e08b1c7b52f3bad7810d2
elecpiano <elecpiano@gmail.com>
2014-07-23 19:58:20 +0800

    initial upload

---
+++ UnityKinectHolo/Assets/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInput();
+    }
+
+    void UpdateInput()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(0.2f, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(-0.2f, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0f, 0f, -0.2f);
+            camera.fieldOfView -= 0.2f;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0f, 0f, 0.2f);
+            camera.fieldOfView += 0.2f;
+        }
+    }
+}
---
+++ UnityKinectHolo/Assets/CameraController3.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController3 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInput();
+    }
+
+    void UpdateInput()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(-0.2f, 0f, 0f);
+            this.transform.Rotate(0f, 1.2f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(0.2f, 0f, 0f);
+            this.transform.Rotate(0f, -1.2f, 0f);
+        }
+    }
+}
